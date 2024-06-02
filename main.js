24d9f1cf04ee881f49d44c82908bc602ab4610bb
Tala99 <hethnawi.tala@gmail.com>
2024-06-02 21:25:10 +0300

    done

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelector(".userProfile h2");
+console.log(document.querySelector(".userProfile h2"));
+document.querySelector(".userProfile h2").textContent="hello tala";
+console.log(document.querySelector(".userinfo p"));
+document.querySelector(".userinfo p").classList.add("dark");
